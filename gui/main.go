8bb8231e06d3a60d3f54f2f8496b1155ca9464d4
jtdv01 <jtdv01@gmail.com>
2022-04-09 16:28:49 +1000

    Form

--- gui/main.go
+++ gui/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "log"
     "image/color"
 
     "fyne.io/fyne/v2"
@@ -8,14 +9,28 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello Future!")
 
-    text1 := canvas.NewText("Hello", color.NRGBA{R: 255, G: 255, B: 255, A:255 })
-	content := container.NewWithoutLayout(text1)
+	entry := widget.NewEntry()
+	textArea := widget.NewMultiLineEntry()
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Author", Widget: entry},
+			{Text: "", Widget: textArea}},
+		OnSubmit: func() { // optional, handle form submission
+			log.Println("Form submitted:", entry.Text)
+			log.Println("multiline:", textArea.Text)
+		},
+	}
+
+    text1 := canvas.NewText("Hashmeme", color.NRGBA{R: 255, G: 255, B: 255, A:255 })
+	content := container.New(layout.NewGridLayout(2), text1, form)
 
 	w.SetContent(content)
 	w.Resize(fyne.NewSize(1200, 700))
