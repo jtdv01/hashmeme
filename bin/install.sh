ff8152d39d863d9c89dd7bf0cfe360a8bb94c542
jtdv01 <jtdv01@gmail.com>
2022-04-09 16:01:29 +1000

    install

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
 main() {
+  # Check if nix is installed
+  command -v nix >/dev/null && \
+    echo "nix installed" \
+    || echo "Command nix nout found. Please install nix: https://nixos.org/download.html" \
+    && exit 1
+
   # Install nixGL for GUI
   nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl && nix-channel --update
   nix-env -iA nixgl.auto.nixGLDefault
 
-  nix-shell --pure --run "echo Installation succesful!"
+  nix-shell --pure --run "echo Installation successful!"
 }
 
 main
