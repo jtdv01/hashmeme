ce4d307219ff1a68488ac55fb5b26db0f6a0d0ec
jtdv01 <jtdv01@gmail.com>
2022-04-09 16:16:46 +1000

    gui form

--- gui/main.go
+++ gui/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+    "image/color"
+
+    "fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello Future!")
 
-	w.SetContent(widget.NewLabel("Hello Future!"))
+    text1 := canvas.NewText("Hello", color.NRGBA{R: 255, G: 255, B: 255, A:255 })
+	content := container.NewWithoutLayout(text1)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(1200, 700))
 	w.ShowAndRun()
 }
