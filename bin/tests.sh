d48ae2a46a7d819882568801329e93e816857dce
jtdv01 <jtdv01@gmail.com>
2022-04-11 20:36:38 +1000

    fix go

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+git_root=$(git rev-parse --show-toplevel)
+
 main () {
   rm -rf ./target
   mkdir ./target
 
   if command -v nix &> /dev/null
   then
-    nix-shell --pure --keep GOROOT ./default.nix --run \
+    nix-shell --pure "${git_root}"/default.nix --run \
       "go test image_processor/*.go"
   else
     echo "Nix not found. See ./bin/install.sh."
     echo "Trying to build without nix, assumes you have go and dependencies installed..."
-    go test image_processor/*.go
+    go test "${git_root}"/image_processor/*.go
   fi
 }
 
